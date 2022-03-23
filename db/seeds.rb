50eb21a14eb73024404d18fe5b0feda1eaea4e3c
Virginie Boutal <boutal.virginie@gmail.com>
2022-03-23 15:58:00 +0100

    Yelp MVP 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Delete restaurants'
+
+Restaurant.destroy_all
+
+puts 'create database'
+
+Restaurant.create(name: "Dan. B", address: "13122 Ventabren" , category: "french")
+Restaurant.create(name: "Le Carthage" , address: "Salon-de-Provence" , category: "belgian")
+Restaurant.create(name: "Chez Louise" , address: "13680 Lançon-Provence", category: "french")
+Restaurant.create(name: "Koi-shi", address: "13001 Marseille" , category: "japanese")
+Restaurant.create(name: "Little Italy", address: "13680 Lançon-Provence" , category: "italian")
+
+puts 'seeds loaded !'
